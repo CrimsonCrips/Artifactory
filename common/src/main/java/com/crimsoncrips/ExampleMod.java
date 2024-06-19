f036e3d659022a4f6c5c5d0b0453261aff94e129
Crim <danecarmona2@gmail.com>
2024-06-20 00:24:42 +0800

    uh?

---
+++ common/src/main/java/com/crimsoncrips/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.crimsoncrips;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "artifactory";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
